9d94ade958035ff7f466be5b4193705a74315f6b
dkhoi1302 <5001104077@student.hcmue.edu.vn>
2025-11-04 19:58:58 +0700

    Update Form1.cs

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -10,7 +10,7 @@ using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-// Hello
+// Hello world
 namespace SnakeGame
 {
     public partial class Form1 : Form
